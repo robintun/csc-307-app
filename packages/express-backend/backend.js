2ff5ef54e9dd0d599b9eeea3bd55c097a4431ed7
robintun <robintun01@gmail.com>
2024-01-30 12:57:22 -0800

    Step 5 Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,22 +42,39 @@ app.get("/", (req, res) => {
 //     res.send(users);
 //   });
 
+// Getting users by name helper
 const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
+
+// Getting users by ID helper
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
   
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
+// Getting users by name
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
     } else {
       res.send(users);
     }
   });
+
+// Getting users by ID
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(port, () => {
   console.log(
