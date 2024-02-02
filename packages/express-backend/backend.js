8afede258e0ffb2838c7ff1c6c6729c897c600c3
robintun <robintun01@gmail.com>
2024-02-02 00:16:24 -0800

    Exercise 3 Step 2 Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -32,7 +33,8 @@ const users = {
     ]
   };
 
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
