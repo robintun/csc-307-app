b82eb9b1227887979232df89e22b92a0b4f7023d
robintun <robintun01@gmail.com>
2024-02-08 16:33:24 -0800

    exercise 4 done trial 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,35 +4,35 @@ import userFuncs from "./user-services.js"
 
 const app = express();
 const port = 8000;
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-  };
+// const users = {
+//     users_list: [
+//       {
+//         id: "xyz789",
+//         name: "Charlie",
+//         job: "Janitor"
+//       },
+//       {
+//         id: "abc123",
+//         name: "Mac",
+//         job: "Bouncer"
+//       },
+//       {
+//         id: "ppp222",
+//         name: "Mac",
+//         job: "Professor"
+//       },
+//       {
+//         id: "yat999",
+//         name: "Dee",
+//         job: "Aspring actress"
+//       },
+//       {
+//         id: "zap555",
+//         name: "Dennis",
+//         job: "Bartender"
+//       }
+//     ]
+//   };
 
   app.use(cors());
   app.use(express.json());
@@ -56,15 +56,19 @@ app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
 
   userFuncs.findUserById(id)
-    .then((result) => {res.send(result)})
+    .then((result) => {
+      if (result) res.send(result);
+      else res.status(404).send(`${id} : Not found`);
+    })
     .catch((error) => {
-      console.log(error)
-      res.status(404).send("Resource not found.");
+      // console.log(error)
+      // res.status(404).send("Resource not found.");
+      res.status(500).send(error.name);
     })
 });
 
 // Using the POST method
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const userToAdd = req.body;
 
   userFuncs.addUser(userToAdd)
